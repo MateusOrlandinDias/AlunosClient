ac568266cb8f51be824c731a59e0c8af73d1eb74
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2023-02-07 21:50:54 -0300

    Migrations adicionada e string connection estabelecida

--- AlunosApi/Context/AppDbContext.cs
+++ AlunosApi/Context/AppDbContext.cs
@@ -11,5 +11,25 @@ namespace AlunosApi.Context
         }
 
         public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria da Penha",
+                    Email = "mariapenha@yahoo.com",
+                    Idade = 23
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Manuel Bueno",
+                    Email = "manuelbueno@yahoo.com",
+                    Idade = 22
+                }
+             );
+        }
     }
 }
