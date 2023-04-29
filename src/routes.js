422fd23758926e9d2088a3fa0425b58784651131
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2023-04-29 20:09:11 -0300

    Class 13\ Implementing Alunos page -Alunos Page -New route setted -Alunos Page style.css done

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './pages/Login';
+import Alunos from './pages/Alunos';
 
 export default function RoutesApp(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={<Login />} />
+                <Route path="/alunos" exact element={<Alunos />} />
             </Routes>
         </BrowserRouter>
     );
