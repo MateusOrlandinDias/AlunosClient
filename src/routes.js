903aa422aff003dedf6388e35f361ccc8625e16c
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2023-05-07 17:15:39 -0300

    Pagina novos alunos com route

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './pages/Login';
 import Alunos from './pages/Alunos';
+import NovoAluno from './pages/NovoAluno';
 
-export default function RoutesApp(){
-    return(
+export default function RoutesApp() {
+    return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={<Login />} />
                 <Route path="/alunos" exact element={<Alunos />} />
+                <Route path="/aluno/novo/:alunoId" exact element={<NovoAluno />} />
             </Routes>
         </BrowserRouter>
     );
